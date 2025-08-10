e6e9842bf5fdf1ac95171812f7fa575db150cc8f
Bhavik Prajapati <rahulprajapati1665@gmail.com>
2025-08-10 08:45:04 +0530

    add

--- utils/response.js
+++ utils/response.js
@@ -1,16 +1,18 @@
 // utils/response.js
 const success = (res, message, data = {}, statusCode = 200) => {
     return res.status(statusCode).json({
+        status:statusCode,
         success: true,
-        message,
+        message:message,
         data
     });
 };
 
 const error = (res, message, statusCode = 500, errors = {}) => {
     return res.status(statusCode).json({
+        status:statusCode,
         success: false,
-        message,
+        message:message,
         errors
     });
 };
